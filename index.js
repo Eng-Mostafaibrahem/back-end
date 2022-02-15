6d20a5db3070b8c19ac15470c1119b1b87ef8870
Mostafa Ibrahem <67388809+Eng-Mostafaibrahem@users.noreply.github.com>
2022-02-15 02:38:29 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const user=require("./routes/user");
+const auth=require("./routes/auth");
+const express=require('express');
+const app = express();
+
+//mongodb+srv://admin:project123@cluster0.mqiiy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+mongoose.connect("mongodb://localhost:27017/project",{ useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("successfull connect with database"))
+    .catch(err => console.error("failed...somthing wrong", err));
+
+app.use(express.json());
+app.use("compression");
+app.use("/api/users",user);
+app.use("/api/auth",auth);
+
+
+const port = process.env.port || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
